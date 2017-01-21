53c9c69e68efb8aa933a273201b8c6ec5596c1ce
Juan Gonzalez <j-gonzalez@email.de>
2017-01-21 21:13:12 +0700

    Add autotools build system

---
+++ clean.sh
@@ -0,0 +1,24 @@
+#! /bin/sh -e
+
+rm -f src/docklight
+rm -f src/*.o
+rm -f src/Makefile src/Makefile.in 
+
+ls -1 | egrep -v "^(debian|m4|bin|po|src|data|man|clean.sh|configure.ac|autogen.sh|autogen2.sh|Makefile.am|nbproject)$"  | xargs rm -r
+
+cd m4
+ls -1 | egrep -v "^(NOTES)$"  | xargs rm -r	
+
+
+cd ../po
+ls -1 | egrep -v "^(de.po|hu.po|LINGUAS|POTFILES.in|compile_all.sh)$"  | xargs rm -r
+
+
+cd ../data
+ls -1 | egrep -v "^(docklight.home.ico|docklight.ini|docklight.logo.png|docklight.launcher.sh|Makefile.am|docklight.menu.desktop|org.freedesktop.docklight.policy)$"  | xargs rm -r
+
+
+
+exit 1
+
+
