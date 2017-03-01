465145c15b78272f3747985b7bb8f7d63bf32fe1
Juan Gonzalez <j-gonzalez@email.de>
2017-03-01 22:14:37 +0700

    Remove echo from launcher script.

--- data/docklight.launcher.sh
+++ data/docklight.launcher.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-    echo " command line contains 0 arguments"
+#    echo " command line contains 0 arguments"
     exit -1	
 fi
 
-echo "$1"
+#echo "$1"
 mv "$1" /usr/share/applications
 
 
